7b5167a24c48cc8c07431a398f3462dce694e27b
Dandan Drori <55210781+dandan-drori@users.noreply.github.com>
2026-07-18 17:49:34 +0300

    fix: missing slashes

--- src/config/cors-options.js
+++ src/config/cors-options.js
@@ -1,4 +1,4 @@
 export const CORS_OPTIONS = {
     credentials: true,
-    origin: ['https://clarity-ai-deploy.vercel.app', 'http:localhost:5173'],
+    origin: ['https://clarity-ai-deploy.vercel.app', 'http://localhost:5173'],
 }
